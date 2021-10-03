714bce964862b1d3d8bdfd499e1e32ceb2c5d6e7
Onur <onr.k@hotmail.com>
2021-10-03 03:40:36 +0300

    PatrolAI Version2

--- Assets/Scripts/Enemy_behaviour.cs
+++ Assets/Scripts/Enemy_behaviour.cs
@@ -11,9 +11,11 @@ public class Enemy_behaviour : MonoBehaviour
     public float attackDistance; //Minimum distance for attack
     public float moveSpeed;
     public float timer; //Timer for cooldown between attakcs
+    public Transform leftLimit;
+    public Transform rightLimit;
 
     private RaycastHit2D hit;
-    private GameObject target;
+    private Transform target;
     private Animator anim;
     private float distance;
     private bool attackMode;
@@ -26,38 +28,57 @@ public class Enemy_behaviour : MonoBehaviour
     {
         intTimer = timer; //Store the inital value of timer
         anim = GetComponent<Animator>();
+        SelectTarget();
+
 
     }
 
     
     void Update()
     {
+        if (!attackMode)
+        {
+
+            Move();
+        }
+
+        if(!InsideofLimits() && !inRange && !anim.GetCurrentAnimatorStateInfo(0).IsName("PatrolAI_attack"))
+        {
+
+            SelectTarget();
+
+        }
+
+
         if(inRange)
         {
 
             hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, raycastMask);
             RaycastDebugger();
-
+            moveSpeed = 6;
+            anim.SetBool("inRange", true);
 
 
         }
-        //when player detected
+        //when raycast is not null
         if(hit.collider != null)
         {
 
             EnemyLogic();
 
         }
-        else if(hit.collider == null)
+        //if nothing is in range
+        else if(hit.collider == null) 
         {
-
+            moveSpeed = 2;
             inRange = false;
         }
 
         if(inRange == false)
         {
-            anim.SetBool("canWalk", false);
+            moveSpeed = 2;
             StopAttack();
+            anim.SetBool("inRange", false);
           
         }
 
@@ -67,8 +88,12 @@ public class Enemy_behaviour : MonoBehaviour
     {
         if(trig.gameObject.tag == "Player")
         {
-            target = trig.gameObject;
+
+            //detect player
+            target = trig.transform;
             inRange = true;
+
+            Flip();
 
 
         }
@@ -81,14 +106,14 @@ public class Enemy_behaviour : MonoBehaviour
         if(distance > attackDistance)
         {
 
-            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+            Debug.DrawRay(rayCast.position, transform.right * rayCastLength, Color.red);
 
 
         }
         else if(attackDistance > distance)
         {
 
-            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.green);
+            Debug.DrawRay(rayCast.position, transform.right * rayCastLength, Color.green);
 
         }
 
@@ -98,11 +123,11 @@ public class Enemy_behaviour : MonoBehaviour
     void EnemyLogic()
     {
 
-        distance = Vector2.Distance(transform.position, target.transform.position);
+        distance = Vector2.Distance(transform.position, target.position);
 
         if(distance > attackDistance)
         {
-            Move();
+           
             StopAttack();
             //methiewwashere
 
@@ -117,7 +142,7 @@ public class Enemy_behaviour : MonoBehaviour
         }
         if (cooling)
         {
-
+            Cooldown();
             anim.SetBool("Attack", false);
         }
 
@@ -127,7 +152,9 @@ public class Enemy_behaviour : MonoBehaviour
         anim.SetBool("canWalk", true);
         if (!anim.GetCurrentAnimatorStateInfo(0).IsName("PatrolAI_attack"))
         {
-            Vector2 targetposition = new Vector2(target.transform.position.x, transform.position.y);
+            //player position
+            Vector2 targetposition = new Vector2(target.position.x, transform.position.y);
+            //Move to player
             transform.position = Vector2.MoveTowards(transform.position, targetposition, moveSpeed * Time.deltaTime);
 
 
@@ -156,8 +183,78 @@ public class Enemy_behaviour : MonoBehaviour
 
 
     }
+    private bool InsideofLimits()
+    {
+        return transform.position.x > leftLimit.position.x && transform.position.x < rightLimit.position.x;
 
 
+    }
+
+  private void SelectTarget()
+    {
+        //puriandWasHere and jiriaeruWasHere
+
+        float distanceToLeft = Vector2.Distance(transform.position, leftLimit.position);
+        float distanceToRight = Vector2.Distance(transform.position, rightLimit.position);
+
+        if(distanceToLeft > distanceToRight)
+        {
+
+            target = leftLimit;
+
+        }
+        else
+        {
+            target = rightLimit;
+
+        }
+
+        Flip();
+
+    }
+
+    private void Flip()
+    {
+
+        Vector3 rotation = transform.eulerAngles;
+        if(transform.position.x > target.position.x)
+        {
+
+            rotation.y = 180f;
+
+        }
+        else
+        {
+
+            rotation.y = 0f;
+
+        }
+
+        transform.eulerAngles = rotation;
 
 
+    }
+
+    void Cooldown()
+    {
+
+        timer -= Time.deltaTime;
+
+
+        if(timer <= 0 && cooling && attackMode)
+        {
+
+            cooling = false;
+            timer = intTimer;
+        }
+
+    }
+
+    public void TriggerCooling()
+    {
+
+        cooling = true;
+
+
+    }
 }
